a9d1bb9c1a553fcd5dcf9c7e4dd8e79f19acecd3
Davide Malavenda <davide.malavenda26@gmail.com>
2024-11-03 16:10:20 +0100

    Initial commit

---
+++ src/main/java/davide/project_week_6_backend/ProjectWeek6BackendApplication.java
@@ -0,0 +1,13 @@
+package davide.project_week_6_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectWeek6BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectWeek6BackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/davide/project_week_6_backend/ProjectWeek6BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package davide.project_week_6_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectWeek6BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
